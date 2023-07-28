f3a8b585cd5b0932cf5fdf52667fc736a385044b
Vishal Agrawal <vishalagrawal@sharechat.com>
2023-07-28 14:49:31 +0530

    Small changes

--- theme/keywind/login/resources/dist/index.js
+++ theme/keywind/login/resources/dist/index.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 import{m as o}from"./assets/module.esm-9a996e1c.js";window.Alpine=o;o.start();
+=======
+import{m as o}from"./assets/module.esm-a89a43e5.js";window.Alpine=o;o.start();
+>>>>>>> 783a0ab (Small changes)
